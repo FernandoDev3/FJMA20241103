74514bc4d953e2a063bbb644ad0335043418b510
Fernando Jose Majano Aguirre <fernando13522477@outlook.com>
2024-03-11 08:36:04 -0600

    Agregar archivos de proyecto.

---
+++ FJMA20241103/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FJMA20241103.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
