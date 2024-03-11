205288dfa750a13d8c3d422cb522752b4c4d78b6
Fernando Jose Majano Aguirre <fernando13522477@outlook.com>
2024-03-11 08:56:22 -0600

    Se crearon los modelos principales

---
+++ FJMA20241103/Models/Auto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FJMA20241103.Models
+{
+    public partial class Auto
+    {
+        public Auto()
+        {
+            ComponenteCarros = new HashSet<ComponenteCarro>();
+        }
+
+        public int IdAuto { get; set; }
+        public string? Marca { get; set; }
+        public string? Modelo { get; set; }
+        public int? Anio { get; set; }
+
+        public virtual ICollection<ComponenteCarro> ComponenteCarros { get; set; }
+    }
+}
---
+++ FJMA20241103/Models/ComponenteCarro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FJMA20241103.Models
+{
+    public partial class ComponenteCarro
+    {
+        public int IdComponente { get; set; }
+        public int? IdAuto { get; set; }
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
+
+        public virtual Auto? IdAutoNavigation { get; set; }
+    }
+}
