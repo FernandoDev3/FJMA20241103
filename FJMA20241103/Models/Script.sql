9c68f58ff4701ada63d728c1eeefaf206f9efea2
Fernando Jose Majano Aguirre <fernando13522477@outlook.com>
2024-03-11 08:39:22 -0600

    Se creò el script para la base de datos

---
+++ FJMA20241103/Models/Script.sql
@@ -0,0 +1,19 @@
+﻿Create database FJMA20241103DB
+
+use FJMA20241103DB
+
+CREATE TABLE Autos (
+    idAuto INT Identity PRIMARY KEY,
+    marca VARCHAR(50),
+    modelo VARCHAR(50),
+    anio INT
+);
+
+-- Crear la tabla ComponenteCarro
+CREATE TABLE ComponenteCarro ( 
+    idComponente INT Identity PRIMARY KEY,
+	idAuto INT,
+    nombre VARCHAR(50),
+    descripcion VARCHAR(255),
+	foreign key (idAuto) references Autos(idAuto)
+);
